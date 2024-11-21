fb1a0b8a95f6d5a54525e3497ed6983c2e6fb0b0
SchneiderCode1 <165057013+SchneiderCode1@users.noreply.github.com>
2024-11-21 01:00:02 -0300

    Jogo Funcionando

--- Game.py
+++ Game.py
@@ -1,7 +1,8 @@
 from package import *
+import colorama
 
 jogada = ProxJogada()
-EndGame = FimDeJogo()
+fim = FimDeJogo()
 
 jogada.RandomPos()
 jogada.PrintBoard()
@@ -9,18 +10,45 @@ jogada.PrintBoard()
 p = 1
 
 while p == 1:
-    proxima_jogada = int(input("digite 1 para subir todas as linhas:\n"))
+    fim.Fim()
+    if fim.endGame:
+        print("Fim de Jogo")
+        break
+    
+    if jogada.endGame:
+        print("Fim de jogo!")
+        break
+        
+        
+    proxima_jogada = input("""
+            +---+
+            | w |
+        +---+---+---+
+        | a | s | d |
+        +---+---+---+
 
-    if proxima_jogada == 1:
+    Digite sua jogada (w/cima, s/baixo, a/esquerda, d/direita) ou 0 para sair: 
+    """).lower()
+
+    if proxima_jogada == "w":
         jogada.SomaUp()
+        
+    elif proxima_jogada == "s":
+        jogada.SomaDown()
+        
+    elif proxima_jogada == "a":
+        jogada.SomaLeft()
+        
+    elif proxima_jogada == "d":
+        jogada.SomaRight()
+        
+    elif proxima_jogada == 0:
+        p == 0
+    
+    else:
+        print("Digite uma jogada válida:\n")
     
     jogada.PrintBoard()
     jogada.RandomPos()
-    terminar_jogo = int(input("Para terminar digite 0\n"))
-    
-    if terminar_jogo == 0:
-        p == 0
-    else:
-        pass
-
     jogada.PrintBoard()
+    
--- package.py
+++ package.py
@@ -1,4 +1,5 @@
 import random
+from colorama import Fore, Style, init
 
 class Grade():
     def __init__(self):
@@ -21,18 +22,25 @@ class RandomPosNum(Grade):
     def __init__(self):
         super().__init__()
         self.lista_pos = self.CriaListaPos()
+        self.endGame = False
     
     def RandomPos(self):
         pos_index = random.randint(0, len(self.lista_pos)-1)
         pos_num = True
-        
+        cont = 0
         while pos_num == True:
+            if cont >=25:
+                self.endGame = True
+                print("Game over! Não é possível adicionar mais nenhum número!")
+                return
+            
             try:
                 if self.lista_pos[pos_index] == 0:
                     self.lista_pos[pos_index] += 2
                     pos_num = False
                 else:
                     pos_index = random.randint(0, len(self.lista_pos)-1)
+                    cont += 1
             except Exception:
                 pos_num = True
     
@@ -42,40 +50,141 @@ class OperNum(RandomPosNum):
         super().__init__()
         
     def SomaUp(self):
-        for i in range(0, len(self.lista_pos)-1):
-            try:
-                #metodo de soma 
-            except IndexError:
-                pass 
-            
+        for z in range(4):
+            for i in range(5, 10):
+                if self.lista_pos[i-5] == 0 or self.lista_pos[i-5] == self.lista_pos[i]: 
+                    self.lista_pos[i-5] += self.lista_pos[i]
+                    self.lista_pos[i] = 0
+                    
+            for i in range(10, 15):
+                if self.lista_pos[i-5] == 0 or self.lista_pos[i-5] == self.lista_pos[i]: 
+                    self.lista_pos[i-5] += self.lista_pos[i]
+                    self.lista_pos[i] = 0
+                    
+            for i in range(15, 20):
+                if self.lista_pos[i-5] == 0 or self.lista_pos[i-5] == self.lista_pos[i]: 
+                    self.lista_pos[i-5] += self.lista_pos[i]
+                    self.lista_pos[i] = 0
+                    
+            for i in range(20, 25): #Linha 5
+                        #linha 5 -> 4, [i-5] = Linha 4
+                        if self.lista_pos[i-5] == 0 or self.lista_pos[i-5] == self.lista_pos[i]:
+                            self.lista_pos[i-5] += self.lista_pos[i] 
+                            self.lista_pos[i] = 0
+                
         return self.lista_pos
     
+    def SomaDown(self):
+        for z in range(4):
+            for i in range(15, 20):
+                if self.lista_pos[i+5] == 0 or self.lista_pos[i+5] == self.lista_pos[i]:
+                    self.lista_pos[i+5] += self.lista_pos[i]
+                    self.lista_pos[i] = 0
+                
+            for i in range(10, 15):
+                if self.lista_pos[i+5] == 0 or self.lista_pos[i+5] == self.lista_pos[i]:
+                    self.lista_pos[i+5] += self.lista_pos[i]
+                    self.lista_pos[i] = 0
+            
+            for i in range(5, 10):
+                if self.lista_pos[i+5] == 0 or self.lista_pos[i+5] == self.lista_pos[i]:
+                    self.lista_pos[i+5] += self.lista_pos[i]
+                    self.lista_pos[i] = 0
+            
+            for i in range(0, 5):
+                if self.lista_pos[i+5] == 0 or self.lista_pos[i+5] == self.lista_pos[i]:
+                    self.lista_pos[i+5] += self.lista_pos[i]
+                    self.lista_pos[i] = 0        
+        return self.lista_pos
+    
+    def SomaLeft(self):
+        for z in range(4):
+            for i in range(1, 22, 5):
+                if self.lista_pos[i-1] == 0 or self.lista_pos[i-1] == self.lista_pos[i]:
+                    self.lista_pos[i-1] += self.lista_pos[i]
+                    self.lista_pos[i] = 0
 
+            for i in range(2, 23, 5):
+                if self.lista_pos[i-1] == 0 or self.lista_pos[i-1] == self.lista_pos[i]:
+                    self.lista_pos[i-1] += self.lista_pos[i]
+                    self.lista_pos[i] = 0    
+                    
+            for i in range(3, 24, 5):
+                if self.lista_pos[i-1] == 0 or self.lista_pos[i-1] == self.lista_pos[i]:
+                    self.lista_pos[i-1] += self.lista_pos[i]
+                    self.lista_pos[i] = 0    
+                    
+            for i in range(4, 25, 5):
+                if self.lista_pos[i-1] == 0 or self.lista_pos[i-1] == self.lista_pos[i]:
+                    self.lista_pos[i-1] += self.lista_pos[i]
+                    self.lista_pos[i] = 0  
+        
+        return self.lista_pos
+    
+    def SomaRight(self):
+        for z in range(4):
+            for i in range(3, 24, 5):
+                if self.lista_pos[i+1] == 0 or self.lista_pos[i+1] == self.lista_pos[i]:
+                    self.lista_pos[i+1] += self.lista_pos[i]
+                    self.lista_pos[i] = 0
+                    
+            for i in range(2, 23, 5):
+                if self.lista_pos[i+1] == 0 or self.lista_pos[i+1] == self.lista_pos[i]:
+                    self.lista_pos[i+1] += self.lista_pos[i]
+                    self.lista_pos[i] = 0  
 
+            for i in range(1, 22, 5):
+                if self.lista_pos[i+1] == 0 or self.lista_pos[i+1] == self.lista_pos[i]:
+                    self.lista_pos[i+1] += self.lista_pos[i]
+                    self.lista_pos[i] = 0   
+
+            for i in range(0, 21, 5):
+                if self.lista_pos[i+1] == 0 or self.lista_pos[i+1] == self.lista_pos[i]:
+                    self.lista_pos[i+1] += self.lista_pos[i]
+                    self.lista_pos[i] = 0       
+        return self.lista_pos
+    
 class ProxJogada(OperNum):
     def __init__(self):
         super().__init__()
     
+    def vermelho(self, texto):
+        return f"{Fore.RED}{texto}{Fore.RESET}"  # Usa a cor vermelha para 0.
+
+    def verde(self, texto):
+        return f"\033[38;2;154;255;0m{texto}\033[0m"  # Usa a cor verde para outros números.
+
+    def formatar_numero(self, numero):
+        num_formatado = f"{numero}".rjust(4)  # Ajuste para garantir que o número ocupe 4 espaços.
+        return self.vermelho(num_formatado) if numero == 0 else self.verde(num_formatado)
+    
     def PrintBoard(self):
         print(f"""
-            -------------------------------
-            |  {self.lista_pos[0]}  |  {self.lista_pos[1]}  |  {self.lista_pos[2]}  |  {self.lista_pos[3]}  |  {self.lista_pos[4]}  |
-            -------------------------------
-            |  {self.lista_pos[5]}  |  {self.lista_pos[6]}  |  {self.lista_pos[7]}  |  {self.lista_pos[8]}  |  {self.lista_pos[9]}  |
-            -------------------------------
-            |  {self.lista_pos[10]}  |  {self.lista_pos[11]}  |  {self.lista_pos[12]}  |  {self.lista_pos[13]}  |  {self.lista_pos[14]}  |
-            -------------------------------
-            |  {self.lista_pos[15]}  |  {self.lista_pos[16]}  |  {self.lista_pos[17]}  |  {self.lista_pos[18]}  |  {self.lista_pos[19]}  |
-            -------------------------------
-            |  {self.lista_pos[20]}  |  {self.lista_pos[21]}  |  {self.lista_pos[22]}  |  {self.lista_pos[23]}  |  {self.lista_pos[24]}  | 
-            -------------------------------
-            """)
+        -----------------------------------------------
+        |  {self.formatar_numero(self.lista_pos[0])}  |  {self.formatar_numero(self.lista_pos[1])}  |  {self.formatar_numero(self.lista_pos[2])}  |  {self.formatar_numero(self.lista_pos[3])}  |  {self.formatar_numero(self.lista_pos[4])}  |
+        -----------------------------------------------
+        |  {self.formatar_numero(self.lista_pos[5])}  |  {self.formatar_numero(self.lista_pos[6])}  |  {self.formatar_numero(self.lista_pos[7])}  |  {self.formatar_numero(self.lista_pos[8])}  |  {self.formatar_numero(self.lista_pos[9])}  |
+        -----------------------------------------------
+        |  {self.formatar_numero(self.lista_pos[10])}  |  {self.formatar_numero(self.lista_pos[11])}  |  {self.formatar_numero(self.lista_pos[12])}  |  {self.formatar_numero(self.lista_pos[13])}  |  {self.formatar_numero(self.lista_pos[14])}  |
+        -----------------------------------------------
+        |  {self.formatar_numero(self.lista_pos[15])}  |  {self.formatar_numero(self.lista_pos[16])}  |  {self.formatar_numero(self.lista_pos[17])}  |  {self.formatar_numero(self.lista_pos[18])}  |  {self.formatar_numero(self.lista_pos[19])}  |
+        -----------------------------------------------
+        |  {self.formatar_numero(self.lista_pos[20])}  |  {self.formatar_numero(self.lista_pos[21])}  |  {self.formatar_numero(self.lista_pos[22])}  |  {self.formatar_numero(self.lista_pos[23])}  |  {self.formatar_numero(self.lista_pos[24])}  |
+        -----------------------------------------------
+    """)
+    
+    def format_number(self, num):
+        return f"{num:4}"
+
 
 class FimDeJogo(ProxJogada):
     def __init__(self):
         super().__init__()
+        self.endGame = False
     
-    def Fim(self, endGame):
+    def Fim(self):
+        cont = 0
         for pos in self.lista_pos:
             if pos == 2048:
-                print("Fim de Jogo")
+                self.endGame == True
+                break
